5a86027138284cdc10dc05fdef5de1f55bedca21
tungmvph <101157223+tungmvph@users.noreply.github.com>
2022-03-19 16:07:13 +0800

    Add files via upload
    ok

---
+++ sukien.js
@@ -0,0 +1,12 @@
+// SỰ KIẾN NÚT BUTTON  CLICK
+// ĐỊNH NGHIA VÀO 1 HÀm
+function onclickbutton(ten_nut) {
+    console.log(ten_nut + ' duoc click');
+}
+
+function onclickopen(value) {
+    if (value == 3) {
+        alert('chucs mừng em đã có được tôi');
+    } else
+        alert('em đã không có được tôi');
+}
